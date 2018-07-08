0f2f8dd17b5e7f062eb3181ac61940457dba1dab
Dillon Coffman <dillonscoffman@gmail.com>
2018-07-08 12:40:50 -0400

    Added components needed, and API call to fetch weather data, used template literals w/API data to allow user to search for specific city weather in a specific country

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const API_KEY = "c9cbb7eb77091b1c59709c84311eaf41"
+
+class App extends React.Component {
+    getWeather = async (e) => {
+        e.preventDefault(); // prevent default behavior of this component on button press
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+        const data = await api_call.json();
+        console.log(data);
+
+    }
+    render() {
+        return (
+            <div>
+                <Titles/>
+                <Form getWeather={this.getWeather}/>
+                <Weather/>
+            </div>
+        );
+    }
 }
 
 export default App;
